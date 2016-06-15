30ec15a0924587e3d35e34f81ff26d0f0e4ba43f
Colin W Kingen and James Howard <james@>
2016-06-15 16:38:46 -0700

    improve endgame behavior and begin AI play construction.

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,6 @@ var currentPlayer = 1;
 var playerOne = new Current();
 var playerTwo = new Current();
 
-var diechecker = function() {
-  if (currentPlayer === 1) {
-    return dice[playerOne.currentRoll - 1];
-  } else {
-    return dice[playerTwo.currentRoll - 1];
-  }
-}
-
-
 function Current() {
   this.currentScore = 0;
   this.currentRoll = 0;
@@ -20,7 +11,6 @@ function Current() {
 Current.prototype.counterReset = function() {
   this.currentScore = 0;
 }
-
 Current.prototype.turn = function() {
   var roll =  Math.floor((Math.random() * 6) + 1 );
   if (roll + this.totalScore >= 100) {
@@ -38,8 +28,46 @@ Current.prototype.turn = function() {
     this.currentScore += this.currentRoll;
     return this.currentRoll;
   }
-} ;
+};
+Current.prototype.computerTurn = function() {
+  var roll =  Math.floor((Math.random() * 6) + 1 );
 
+  if (roll + this.totalScore >= 100) {
+    alert("Player " + currentPlayer + " wins!!");
+    playerOne.totalScore = 0;
+    playerTwo.totalScore = 0;
+    playerOne.currentScore = 0;
+    playerTwo.currentRoll = 0;
+  }
+  this.currentRoll = roll;
+  if (this.currentRoll === 1) {
+     this.currentScore = 0;
+     playerUpdate = playerSwitch(this.currentScore, this.totalScore)
+     currentPlayer = playerUpdate[0];
+
+    //  Super Janky AI First Draft
+  } else if (this.currentScore < 20 ) {
+    this.currentRoll = diceRolls();
+    this.currentScore += this.currentRoll;
+    this.currentRoll = diceRolls();
+    this.currentScore += this.currentRoll;
+  } else if (this.currentScore < 12 ) {
+    this.currentRoll = diceRolls();
+    this.currentScore += this.currentRoll;
+  } else {
+     return this.currentRole;
+  }
+};
+var diceRolls = function() {
+  return Math.floor((Math.random() * 6) + 1);
+}
+var diechecker = function() {
+  if (currentPlayer === 1) {
+    return dice[playerOne.currentRoll - 1];
+  } else {
+    return dice[playerTwo.currentRoll - 1];
+  }
+}
 var playerSwitch = function(playerCurrent, playerTotal) {
   playerTotal += playerCurrent;
   if (currentPlayer === 1 ) {
@@ -50,17 +78,14 @@ var playerSwitch = function(playerCurrent, playerTotal) {
   return [currentPlayer,playerTotal];
 }
 
-
 $(document).ready(function() {
   $("#roll").click(function() {
     if (currentPlayer === 1) {
       playerOne.turn();
-      $("#score-player1").html("<li> Score This Turn: " + playerOne.currentScore + "</li><li> Score This Roll: "
-                                  + playerOne.currentRoll + "</li><li> Grand Total Score: " + playerOne.totalScore + "</li>" );
+      $("#score-player1").html("<li> Score This Turn: " + playerOne.currentScore + "</li><li> Score This Roll: " + playerOne.currentRoll + "</li><li> Grand Total Score: " + playerOne.totalScore + "</li>" );
     } else {
-      playerTwo.turn();
-      $("#score-player2").html("<li> Score This Turn: " + playerTwo.currentScore + "</li><li> Score This Roll: "
-                                  + playerTwo.currentRoll + "</li><li> Grand Total Score: " + playerTwo.totalScore + "</li>");
+      playerTwo.computerTurn();
+      $("#score-player2").html("<li> Score This Turn: " + playerTwo.currentScore + "</li><li> Score This Roll: " + playerTwo.currentRoll + "</li><li> Grand Total Score: " + playerTwo.totalScore + "</li>");
     }
     var diceface = diechecker();
     $("#current-die").html(diceface);
@@ -68,13 +93,11 @@ $(document).ready(function() {
   $("#hold").click(function() {
     if (currentPlayer === 1) {
       var switchReturn1 = playerSwitch(playerOne.currentScore,playerOne.totalScore);
-      // currentPlayer = switchReturn1[0];
       playerOne.totalScore = switchReturn1[1];
       $("#one-score").text("Player One Score : " + playerOne.totalScore);
       playerOne.counterReset();
     } else {
       var switchReturn2 = playerSwitch(playerTwo.currentScore,playerTwo.totalScore);
-      // currentPlayer = switchReturn2[0];
       playerTwo.totalScore = switchReturn2[1];
       $("#two-score").text("Player Two Score : " + playerTwo.totalScore);
       playerTwo.counterReset()
