328f75fa0c6753d1856d416f5a0f7eb323acf2f1
Colin W Kingen and James Howard <james@>
2016-06-16 16:42:08 -0700

    finish AI mode and improve interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,111 +1,84 @@
 var dice = ["&#9856", "&#9857", "&#9858", "&#9859", "&#9860", "&#9861"];
 var currentPlayer = 1;
 var playerOne = new Current();
-var playerTwo = new Current();
-
+var computerPlayer = new Current();
 function Current() {
-  this.currentScore = 0;
   this.currentRoll = 0;
+  this.currentScore = 0;
   this.totalScore = 0;
 };
-Current.prototype.counterReset = function() {
-  this.currentScore = 0;
-}
-Current.prototype.turn = function() {
-  var roll =  Math.floor((Math.random() * 6) + 1 );
-  if (roll + this.totalScore >= 100) {
-    alert("Player " + currentPlayer + " wins!!");
-    this.totalScore = 0;
-    this.currentScore = 0;
-    this.currentRoll = 0;
-  }
-  this.currentRoll = roll;
-  if (this.currentRoll === 1) {
-     this.currentScore = 0;
-     playerUpdate = playerSwitch(this.currentScore, this.totalScore)
-     currentPlayer = playerUpdate[0];
-  } else {
-    this.currentScore += this.currentRoll;
-    return this.currentRoll;
-  }
-};
-Current.prototype.computerTurn = function() {
-  var roll =  Math.floor((Math.random() * 6) + 1 );
-
-  if (roll + this.totalScore >= 100) {
-    alert("Player " + currentPlayer + " wins!!");
-    playerOne.totalScore = 0;
-    playerTwo.totalScore = 0;
-    playerOne.currentScore = 0;
-    playerTwo.currentRoll = 0;
-  }
-  this.currentRoll = roll;
-  if (this.currentRoll === 1) {
-     this.currentScore = 0;
-     playerUpdate = playerSwitch(this.currentScore, this.totalScore)
-     currentPlayer = playerUpdate[0];
-
-    //  Super Janky AI First Draft
-  } else if (this.currentScore < 20 ) {
-    this.currentRoll = diceRolls();
-    this.currentScore += this.currentRoll;
-    this.currentRoll = diceRolls();
-    this.currentScore += this.currentRoll;
-  } else if (this.currentScore < 12 ) {
-    this.currentRoll = diceRolls();
-    this.currentScore += this.currentRoll;
-  } else {
-     return this.currentRole;
-  }
-};
-var diceRolls = function() {
-  return Math.floor((Math.random() * 6) + 1);
-}
 var diechecker = function() {
   if (currentPlayer === 1) {
     return dice[playerOne.currentRoll - 1];
   } else {
-    return dice[playerTwo.currentRoll - 1];
+    return dice[computerPlayer.currentRoll - 1];
   }
 }
-var playerSwitch = function(playerCurrent, playerTotal) {
-  playerTotal += playerCurrent;
-  if (currentPlayer === 1 ) {
-    currentPlayer = 2;
+var diceRoll = function(player) {
+  roll = Math.floor((Math.random() * 6) + 1);
+  if (roll === 1 && player === 1) {
+    playerOne.currentRoll = 0;
+    playerOne.currentScore = 0;
+    return 0;
+  } else if (roll === 1) {
+    computerPlayer.currentRoll = 0;
+    computerPlayer.currentScore = 0;
+    return 0;
+  } else if ( playerOne.totalScore + roll > 100) {
+    alert("playerOne Wins!")
+  } else if ( computerPlayer.totalScore + roll > 100) {
+    alert("ComputerPlayer Wins!")
+  }
+  return roll;
+};
+Current.prototype.runTurn = function() {
+  currentPlayer = 1;
+  var roll = diceRoll(currentPlayer);
+  if (roll === 0) {
+    computerPlayer.compTurn();
+  }
+  this.currentRoll = roll;
+  this.currentScore += this.currentRoll;
+};
+
+Current.prototype.compTurn = function() {
+  currentPlayer = 2;
+  var roll = diceRoll(currentPlayer);
+  if (roll === 0) {
+    playerOne.runTurn();
+  } 
+  while (computerPlayer.currentScore < 15) {
+    roll = diceRoll(2);
+    this.currentRoll = roll;
+    this.currentScore += this.currentRoll;
+  }
+  // this.currentRoll = roll;
+  // this.currentScore += this.currentRoll;
+};
+var totalScore = function(player) {
+  if ( currentPlayer === 1 ) {
+    playerOne.totalScore += playerOne.currentScore;
+    playerOne.currentScore = 0;
   } else {
-    currentPlayer = 1;
-  }
-  return [currentPlayer,playerTotal];
-}
+    computerPlayer.totalScore += computerPlayer.currentScore;
+    computerPlayer.currentScore = 0;
+  };
+};
 
 $(document).ready(function() {
   $("#roll").click(function() {
-    if (currentPlayer === 1) {
-      playerOne.turn();
-      $("#score-player1").html("<li> Score This Turn: " + playerOne.currentScore + "</li><li> Score This Roll: " + playerOne.currentRoll + "</li><li> Grand Total Score: " + playerOne.totalScore + "</li>" );
-    } else {
-      playerTwo.computerTurn();
-      $("#score-player2").html("<li> Score This Turn: " + playerTwo.currentScore + "</li><li> Score This Roll: " + playerTwo.currentRoll + "</li><li> Grand Total Score: " + playerTwo.totalScore + "</li>");
-      playerUpdate = playerSwitch(playerTwo.currentScore,playerTwo.totalScore);
-      currentPlayer = 1;
-    }
+    playerOne.runTurn();
+    $("#score-player1").html("<li> Score This Turn: " + playerOne.currentScore + "</li><li> Score This Roll: " + playerOne.currentRoll + "</li><li> Grand Total Score: " + playerOne.totalScore + "</li>");
+    $("#score-player2").html("<li> Score This Turn: " + computerPlayer.currentScore + "</li><li> Score This Roll: " + computerPlayer.currentRoll + "</li><li> Grand Total Score: " + computerPlayer.totalScore + "</li>");
     var diceface = diechecker();
     $("#current-die").html(diceface);
-
   });
   $("#hold").click(function() {
-    if (currentPlayer === 1) {
-      var switchReturn1 = playerSwitch(playerOne.currentScore,playerOne.totalScore);
-      playerOne.totalScore = switchReturn1[1];
-      $("#one-score").text("Player One Score : " + playerOne.totalScore);
-      playerOne.counterReset();
-      playerTwo.computerTurn()
-    } else {
-      var switchReturn2 = playerSwitch(playerTwo.currentScore,playerTwo.totalScore);
-      playerTwo.totalScore = switchReturn2[1];
-      $("#two-score").text("Player Two Score : " + playerTwo.totalScore);
-      playerTwo.counterReset()
-      playerTwo.computerTurn()
-    };
+    totalScore(currentPlayer);
+    $("#score-player1").html("<li> Score This Turn: " + playerOne.currentScore + "</li><li> Score This Roll: " + playerOne.currentRoll + "</li><li> Grand Total Score: " + playerOne.totalScore + "</li>");
+    computerPlayer.compTurn();
+    $("#score-player2").html("<li> Score This Turn: " + computerPlayer.currentScore + "</li><li> Score This Roll: " + computerPlayer.currentRoll + "</li><li> Grand Total Score: " + computerPlayer.totalScore + "</li>");
+    totalScore(currentPlayer);
+    currentPlayer = 1;
   });
 });
